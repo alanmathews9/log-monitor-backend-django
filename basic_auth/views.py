b96c5234869c36eebe7e5bd5ad701e880ce53a8b
alan <alangeorgemathews09@gmail.com>
2023-03-22 02:48:27 +0530

    Updated api calls

--- basic_auth/views.py
+++ basic_auth/views.py
@@ -10,15 +10,16 @@ logger = logging.getLogger(__name__)
 
 @csrf_exempt
 def login(request):
+    
+    if 'email_id' not in request.POST:
+        logger.error('Got login request without email field. Request: {}.'.format(request.POST))
+        return HttpResponse(json.dumps({"type": "LoginResponse", "status": "failure", "reason": "email id not present"}, default=str), 
+                                        content_type="application/json")
     if 'password' not in request.POST:
         logger.error(
             'Got login request without password field. Request: {}.'.format(request.POST))
         return HttpResponse(json.dumps({"type": "LoginResponse", "status": "failure", "reason": "password id not present"}, default=str),
                             content_type="application/json")
-    if 'email_id' not in request.POST:
-        logger.error('Got login request without email field. Request: {}.'.format(request.POST))
-        return HttpResponse(json.dumps({"type": "LoginResponse", "status": "failure", "reason": "email id not present"}, default=str), 
-                                        content_type="application/json")
 
     email_id = request.POST['email_id']
     password = request.POST['password']
