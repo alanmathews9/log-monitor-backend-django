963cc6549409d9b1db8d2eff9cc4123b9f1cb0a6
alan <alangeorgemathews09@gmail.com>
2023-03-28 21:48:45 +0530

    Created app home

--- home/views.py
+++ home/views.py
@@ -1,8 +1,3 @@
+from django.shortcuts import render
 
-from django.http import JsonResponse
-from .models import log
-
-def get_all_logs(request):
-    logs = log.objects.all().values()  
-    logs_list = list(logs)  
-    return JsonResponse(logs_list,safe=False)
+# Create your views here.
--- mysite/settings.py
+++ mysite/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'home',
-    'basic_auth',
     'corsheaders',
 ]
 
