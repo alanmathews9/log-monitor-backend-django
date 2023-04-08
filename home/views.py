13a5f17462e3c55b30d8d094a97318c3ba135712
alan <alangeorgemathews09@gmail.com>
2023-04-08 18:11:07 +0530

    update get_all_logs

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 
-from django.http import JsonResponse
+from django.http import HttpResponse
+import json
 from .models import log
-
+from django.views.decorators.csrf import csrf_exempt
+@csrf_exempt
 def get_all_logs(request):
-    logs = log.objects.all().values()  
-    logs_list = list(logs)  
-    return JsonResponse(logs_list,safe=False)
+    response_data = { "logs": list(log.objects.values().all()) }
+    return HttpResponse(json.dumps(response_data, default=str), content_type="application/json")
--- mysite/settings.py
+++ mysite/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOWED_ORIGINS = ["http://localhost:8000", "http://127.0.0.1:8000",]
+CORS_ALLOWED_ORIGINS = ["http://localhost:8000", "http://127.0.0.1:8000"]
 
 
 # Log monitor settings
