f64588c7007873843db53a3fe5a0625f7d10e4bf
alan <alangeorgemathews09@gmail.com>
2023-04-14 23:58:02 +0530

    Updated handling api

--- home/views.py
+++ home/views.py
@@ -11,15 +11,15 @@ def get_all_logs(request):
     return HttpResponse(json.dumps(response_data, default=str), content_type="application/json")
 
 @csrf_exempt
-def handle_logs(request, log_id):
-    if request.method == 'POST':
-        email_id = request.POST.get('email_id')
-        log_item = log.objects.filter(id=log_id).first()
-        if log_item is not None:
-            log_item.handled_by = email_id if email_id is not None else ''
-            log_item.handled_time = datetime.datetime.now()
-            log_item.save()
-            response_data = {'status': 'success', 'handled_by': log_item.handled_by, 'handled_time': log_item.handled_time.isoformat()}
-            return HttpResponse(json.dumps(response_data), content_type='application/json')
-    response_data = {'status': 'failure'}
-    return HttpResponse(json.dumps(response_data), content_type='application/json')
+def handle_log(request, log_id):
+    current_user = request.user.email
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    try:
+        l = log.objects.get(id=log_id)
+        l.handled_by = current_user
+        l.handled_time = current_time
+        l.save()
+        response_data = { "status": "success" }
+    except log.DoesNotExist:
+        response_data = { "status": "error", "message": "Log entry not found" }
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
