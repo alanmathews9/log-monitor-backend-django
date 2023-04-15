b418d01bc7712351f86d11b78a96eeb89e76623b
alan <alangeorgemathews09@gmail.com>
2023-04-15 15:09:19 +0530

    Added default and content_type to httpresponse

--- home/views.py
+++ home/views.py
@@ -14,14 +14,17 @@ def get_all_logs(request):
 @csrf_exempt
 def handle_log(request):
     if 'log_id' not in request.data:
-        return HttpResponse(json.dumps({"type": "HandleLogResponse", "status": "failure", "reason": "log_id not present" }))
+        return HttpResponse(json.dumps({"type": "HandleLogResponse", "status": "failure", "reason": "log_id not present" }, default=str),
+                            content_type="application/json")
     if 'comment' not in request.data:
-        return HttpResponse(json.dumps({"type": "HandleLogResponse", "status": "failure", "reason": "comment not present" }))
+        return HttpResponse(json.dumps({"type": "HandleLogResponse", "status": "failure", "reason": "comment not present" }, default=str),
+                            content_type="application/json")
     log_id= request.data['log_id']
     comment = request.data['comment']
-    handled_by = request.User.get_email()
+    handled_by = User.get_email()
     handled_time = datetime.datetime.now()
     update_log = log.objects.filter(id=log_id).update(handled_by=handled_by, handled_time=handled_time, comment=comment)
     if update_log:
-        return HttpResponse(json.dumps({"type": "HandleLogResponse", "status": "success" }))
+        return HttpResponse(json.dumps({"type": "HandleLogResponse", "status": "success" }, default=str),
+                            content_type="application/json")
     
