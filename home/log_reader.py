384954ecd85e4c079d339eb6583dccae32c8ade4
alan <alangeorgemathews09@gmail.com>
2023-05-04 00:11:38 +0530

    Back to normal

--- home/log_reader.py
+++ home/log_reader.py
@@ -57,7 +57,7 @@ def start_all_log_reader_threads(root_dir):
         # Check whether file is in text format or not
         if not file.lower().endswith('.log'):
             continue
-        file_path = f"{root_dir}\sample_log\{file}"
+        file_path = f"{root_dir}\{file}"
         t = LogReaderThread(file_path)
         t.start()
         file_reading_threads.append(t)
