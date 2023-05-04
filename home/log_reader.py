a6d4aa414501ad1f5b913de576beb8fd9e4e321f
alan <alangeorgemathews09@gmail.com>
2023-05-04 15:11:32 +0530

    StrategyServer logs generated

--- home/log_reader.py
+++ home/log_reader.py
@@ -1,46 +1,74 @@
-import time
+# Import Module
+import os
 import threading
-import signal
 import sys
+from zoneinfo import ZoneInfo
 from datetime import datetime
-import random
+from mysite.settings import LOG_STRUCTURE_LIST as log_structure_list
+from home.models import log
 
-kill_pill = False
+import signal
 
-def set_kill_pill():
-    global kill_pill
-    kill_pill = True
+file_reading_threads = []
+
+class LogReaderThread:
+    def __init__(self, filepath):
+        self._kill_pill = False
+        self._thread = None
+
+        self._last_pos = 0
+        self._filepath = filepath
+        self._values_dict = {}
+
+    def start(self):
+        self._thread = threading.Thread(target=self.read_log, args=())
+        self._thread.start()
+
+    def kill(self):
+        print('Killing thread...')
+        self._kill_pill = True
+
+    def read_log(self):
+        global log_structure_list
+        while not self._kill_pill:
+            self._values_dict = {}
+            with open(self._filepath) as f:
+                f.seek(self._last_pos)
+                line = f.readline()
+                while line:
+                    for s in log_structure_list:
+                        start = line.find('[')
+                        end = line.find(']')
+                        self._values_dict[s] = line[start+1:end]
+                        line = line[end+1:]
+                    self._values_dict['message'] = line.strip()
+                    line = f.readline()
+                    self.write_to_db()
+                self._last_pos = f.tell()
+    
+
+    def write_to_db(self):
+        timestamp = datetime.strptime(self._values_dict['timestamp'], '%y-%m-%d %H:%M:%S.%f')
+        application_name = self._values_dict.get('application_name', '')
+        level = self._values_dict.get('level', '')
+        message = self._values_dict.get('message', '')
+        logs = log(timestamp=timestamp.replace(tzinfo=ZoneInfo('Asia/Kolkata')), application_name=application_name, level=level, message=message)
+        logs.save()
+        print(self._values_dict['timestamp'])
+def start_all_log_reader_threads(root_dir):
+    for file in os.listdir(root_dir):
+        # Check whether file is in text format or not
+        if not file.lower().endswith('.log'):
+            continue
+        file_path = f"{root_dir}\{file}"
+        t = LogReaderThread(file_path)
+        t.start()
+        file_reading_threads.append(t)
+
+def kill_all_log_reader_threads(signum, stack_frame):
+    global file_reading_threads
+    for ft in file_reading_threads:
+        ft.kill()
     sys.exit(0)
 
-
-signal.signal(signal.SIGINT, set_kill_pill)
-
-log_msg_list = [ ['WARNING', 'This is a warning message'], ['ERROR', 'This is one error msg'], ['CRITICAL', 'This is a CRITICAL']]
-# appllication_list = ['AppServer', 'StrategyServer', 'NodeServer', 'Algo']
-
-print('Enter Application name: ')
-application = input()
-
-def file_writer(file_path):
-    global kill_pill
-    while True:
-        now = datetime.now() # current date and time
-        localtime=now.strftime("%y-%m-%d %H:%M:%S.%f")
-        level, msg = random.choice(log_msg_list)
-        # application = random.choice(appllication_list)
-        logs='['+ localtime +'] [' + application + '] [' + level + '] ' + msg
-        f=open(file_path,"a")
-        f.write(logs)
-        f.write('\n')
-        f.close()
-        time.sleep(1)
-        #print(kill_pill)
-        if kill_pill:
-            break
-
-file_path = 'sample_logs/' + application +'.log'
-t = threading.Thread(target=file_writer, args=(file_path,))
-t.start()
-a = input()
-set_kill_pill()
-# t.join()
+signal.signal(signal.SIGINT, kill_all_log_reader_threads)
